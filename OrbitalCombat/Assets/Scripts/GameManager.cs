8fb82e23525f966ff0d372377665a79c165b8269
Marius Laptop <marius.muehleck@gmx.de>
2019-06-29 00:04:49 +0200

    * Implements basic Game Manager and basic scoring system

---
+++ OrbitalCombat/Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ [RequireComponent(typeof(ScoreManager))]
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] mapPresets;
+
+    private GameObject mapParent;
+    private GameObject[] planets;
+
+    private void Start()
+    {
+        mapParent = Instantiate(mapPresets[Random.Range(0, mapPresets.Length)], Vector3.zero, Quaternion.identity);
+        planets = new GameObject[mapParent.transform.childCount];
+        for (int j = 0; j < mapParent.transform.childCount; j++)
+        {
+            planets[j] = mapParent.transform.GetChild(j).gameObject;
+            planets[j].GetComponent<PlanetManager>().Init();
+        }
+
+        GetComponent<ScoreManager>().Init(mapParent);
+
+        planets[0].GetComponent<PlanetManager>().SetToTeam(1);
+        planets[2].GetComponent<PlanetManager>().SetToTeam(2);
+        planets[3].GetComponent<PlanetManager>().SetToTeam(2);
+    }
+
+    public void GameOver (int winningTeam)
+    {
+        Time.timeScale = 0.0f;
+
+        if (winningTeam == 1)
+        {
+            Debug.Log("Team Yellow wins!");
+        }
+        else if (winningTeam == 2)
+        {
+            Debug.Log("Team Green wins!");
+        }
+    }
+}
