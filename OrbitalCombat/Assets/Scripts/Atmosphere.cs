4e7f303f19b4794ffad4d63c73def5d108024dd7
Daniel Schulz <faulolio@gmail.com>
2019-06-29 00:25:45 +0200

    planet prefab

---
+++ OrbitalCombat/Assets/Scripts/Atmosphere.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atmosphere : MonoBehaviour {
+    [SerializeField]
+    private AnimationCurve airResistanceOverDistance = default;
+
+    void OnTriggerStay(Collider other) {
+        if (other.attachedRigidbody) {
+            var todo = Vector3.zero;
+            other.attachedRigidbody.AddForce(todo);
+        }
+    }
+}
---
+++ OrbitalCombat/Assets/Scripts/GravityWell.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityWell : MonoBehaviour {
+    [SerializeField]
+    private AnimationCurve gravityOverDistance = default;
+
+    void OnTriggerStay(Collider other) {
+        if (other.attachedRigidbody) {
+            var todo = Vector3.zero;
+            other.attachedRigidbody.AddForce(todo);
+        }
+    }
+}
