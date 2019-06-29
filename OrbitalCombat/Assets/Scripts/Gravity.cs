c908d677fdf74ff1484633210603c7123cc2df1d
Daniel Schulz <faulolio@gmail.com>
2019-06-29 02:03:38 +0200

    gravity!

--- OrbitalCombat/Assets/Scripts/Gravity.cs
+++ OrbitalCombat/Assets/Scripts/Gravity.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 
 public class Gravity : MonoBehaviour {
-    public float radius {
-        get => transform.localScale.x * collider.radius;
-    }
     public new CircleCollider2D collider => GetComponent<CircleCollider2D>();
+    public Planet planet => GetComponentInParent<Planet>();
 
     [SerializeField]
     private AnimationCurve gravityOverDistance = default;
 
     void OnTriggerStay(Collider other) {
         if (other.attachedRigidbody) {
-            var todo = Vector3.zero;
-            other.attachedRigidbody.AddForce(todo);
+            Vector2 directionNorm = (planet.transform.position - transform.position).normalized;
+            float dist = Vector2.Distance(transform.position, other.transform.position);
+            float gravitationForce = gravityOverDistance.Evaluate(dist) * other.attachedRigidbody.mass * planet.coreMass;
+            
+            other.attachedRigidbody.AddForce(directionNorm * gravitationForce);
         }
     }
 }
